40eda0f2e755c3cae146a45c4e1e43e994a225ac
jhojan290 <davio13bar@gmail.com>
2023-12-12 16:53:14 -0500

    primer commit

---
+++ SegundoEjercicio/SegundoEjercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace SegundoEjercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int edad;
+            //int edad1; //Declaración explicita de variables
+            //int edad2;
+            //int edad3;
+
+            /*edad = edad1 = edad2 = edad3 = 26;*/ // manera de asignarle el mismo valor a varias variables a la vez
+
+            // Console.WriteLine(edad);
+
+            var edad = 20; // Declaración implicita de variables.
+
+                           // el compilador deduce según el valor de la variable, el tipo de dato con el que se inicializó
+                           // todo esto usando la palabra var
+
+            Console.WriteLine(edad);
+
+
+        }
+    }
+}
+
+
---
+++ primer-ejercicio/primer-ejercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace PrimerEjercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(5/2); si el resultado de la división es un resultado decimal, solo se mostrará la parte entera
+            // para que se muestre el resultado decimal se hace de la siguiente manera:
+
+            //Console.WriteLine(5.0/2.0);
+
+            //Console.WriteLine(5 / 2.0); Si la división es entre un número entero y un número decimal, el resultado será decimal
+
+            int edad = 20;
+
+            edad += 3; // aumentar el valor de la variable en 3
+
+            // edad++; Incrementa en uno la edad 
+
+            // Console.WriteLine($"Tienes la edad de {edad} años");
+
+            Console.WriteLine("Tiene la edad de " + ++edad + " años"); // para aumentar en uno desde el console.writeline y concatenar los resultados
+            // se usa el simbolo ++ como sufijo para que aumente el valor de edad, ya que c# se lee de derecha a izquierda
+
+
+        }
+    }
+}
+
