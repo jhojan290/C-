5a783c6c7e463f783385140ccab3f568ee3f6969
jhojan290 <davio13bar@gmail.com>
2023-12-13 16:37:42 -0500

    segundo commit

--- SegundoEjercicio/SegundoEjercicio/Program.cs
+++ SegundoEjercicio/SegundoEjercicio/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Timers;
+
 namespace SegundoEjercicio
 {
     class Program
@@ -14,12 +16,39 @@ namespace SegundoEjercicio
 
             // Console.WriteLine(edad);
 
-            var edad = 20; // Declaración implicita de variables.
+            /*var edad = 20;*/ // Declaración implicita de variables.
 
-                           // el compilador deduce según el valor de la variable, el tipo de dato con el que se inicializó
-                           // todo esto usando la palabra var
+            // el compilador deduce según el valor de la variable, el tipo de dato con el que se inicializó
+            // todo esto usando la palabra var
 
-            Console.WriteLine(edad);
+            // A estas variables implicitas no se les puede cambiar su tipo de dato en el código después de ser declaradas
+
+            //Console.WriteLine(edad);
+
+            //--------------------------------------------------------------------------------------------------------------------------------//
+
+            /*Conversión explícita o Casting*/
+
+            //double temperatura = 34.5;
+
+            //int temperaturaM;
+
+            //temperaturaM = (int)temperatura; // almacena en una variable de tipo entero, el valor de la variable tipo double, pero indicando al lado de esta
+            ////el tipo de dato al que quiere ser pasado
+
+            //Console.WriteLine(temperaturaM);
+
+
+            //-------------------------------------------------------------------------------------------------------------------------------//
+
+            /*Conversión Implicita*/
+
+            int habitantes = 1000000;
+
+            long habitanteNuevaCiudad = habitantes; // hace la conversión sin necesidad de casting gracias a que los tipos de valores son compatibles
+            // esto según la tabla de conversiones de valores implícitos
+
+            Console.WriteLine(habitanteNuevaCiudad);
 
 
         }
---
+++ TercerEjercicio/TercerEjercicio/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace TercerEjercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conversión de texto a número
+
+            //Console.WriteLine("Introduce el primer número");
+
+            //int num = int.Parse(Console.ReadLine()); // el dato que se solicita en consola, al pasar al entorno de ejecución va como un string
+            //// con el .Parse se puede convertir este dato en tipo texto a un tipo entero, con la línea anterior
+
+            //Console.WriteLine("Introduce el segundo número");
+
+            //int num1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("el resultado es: " + (num + num1));
+
+
+
+            //----------------------------------------------------------------------------------------------------------------------------
+
+
+            //const int VALOR = 5;
+            //const int VALOR2 = 10; // Declaración de las variables constantes en mayúsculas por recomendación
+
+            //Console.WriteLine("el valor del número es: {0}", VALOR, VALOR2); 
+            // se le pueden pasar parámetros al Console.Writeline, dentro de las comillas se establece con llaves la posición del objeto a mostrar
+
+            const double PI = 3.1416;
+
+            Console.WriteLine("Introduzca la medida del radio");
+
+            double radio = double.Parse(Console.ReadLine());
+
+            double area = radio * radio * PI;
+
+            Console.WriteLine($"El área del circulo es: {area}");
+        }
+    }
+}
+
+
+
