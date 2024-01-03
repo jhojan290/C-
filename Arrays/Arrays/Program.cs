65f8b25eee2f51642b5242c0c00812630ce3f22f
jhojan290 <davio13bar@gmail.com>
2024-01-03 16:50:04 -0500

    commit 03-01

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] edades;
+
+            //edades = new int[4];
+
+            //edades[0] = 15;
+
+            //edades[1] = 27;
+
+            //edades[2] = 19;
+
+            //edades[3] = 80;
+
+            //Console.WriteLine(edades[2]);
+
+            //int[] edades = { 15, 27, 19, 80 };
+
+            //int[] edades = new int [4] { 15, 27, 19, 80 };
+
+            //Console.WriteLine(edades[2]);
+
+            //-----------------------------------------------------------------------------
+
+            // Array Implicito
+
+            /*var datos = new[] { "Juan", "Díaz", 15 }; no son del mismo tipo de dato por lo tanto no acepta los datos en el array*/
+
+            var valores = new[] { 15, 28, 35, 75.5, 30.30 }; /*Todo lo convierte a double*/
+
+            //-----------------------------------------------------------------------------
+
+            // Arrays de Objetos
+
+            Empleados Ana = new Empleados("Ana", 23); // Primera manera de crear un array de objetos creando una instancia de la clase
+
+            Empleados[] arrayEmpleados = new Empleados[2]; // Creación de array de la clase Empleados
+
+            arrayEmpleados[0] = new Empleados("Pedro", 37); // Segunda manera de crear un array de objetos, creando y almacenando un objeto por posicion
+
+            arrayEmpleados[1] = Ana; // Primera manera de crear un array de objetos, poniendo en la posicion 1 el objeto creado en la parte superior
+
+            //-----------------------------------------------------------------------------
+
+            // array de tipos o clases anónimas
+
+            var personas = new[]
+            {
+                new{Nombre="Juan", Edad=19},
+
+                new{Nombre="Maria", Edad= 49},
+
+                new{Nombre="Diana", Edad = 35}
+
+            };
+
+            //Console.WriteLine(personas[1]);
+
+            //for(int i = 0; i <= 4; i++)
+            //{
+            //    Console.WriteLine(valores[i]);
+            //}
+
+            //for (int i = 0; i <= valores.Length; i++) // metodos de la clase array, con .lenght recorro todo el array
+            //{
+            //    Console.WriteLine(valores[i]);
+            //}
+
+            for (int i = 0; i <= arrayEmpleados.Length; i++) // metodos de la clase array, con .lenght recorro todo el array
+            {
+                Console.WriteLine(arrayEmpleados[i]);
+            }
+        }
+
+    }
+
+    class Empleados
+    {
+        public Empleados(String nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+
+        }
+
+        String nombre;
+        int edad;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PooEjemplo4/PooEjemplo4/Program.cs
+++ PooEjemplo4/PooEjemplo4/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using static System.Math; // para importar todos los métodos estaticos de la clase Math 
+using static System.Console; // para importar todos los métodos estaticos de la clase Console
 
 
 namespace PooEjemplo4
@@ -7,10 +8,37 @@ namespace PooEjemplo4
     {
         static void Main(string[] args)
         {
-            RealizarTarea();
+            realizarTarea();
+
+            //double raiz = Sqrt(9);
+
+            //double potencia = Pow(3, 4);
+
+            //WriteLine(raiz);
+
+            //WriteLine(potencia);
+
+            //-----------------------------------------------------------------------------------
+
+            var MiVariable = new { Nombre = "Juan", edad = 19 }; // Clases anonimas donde no se especifica un nombre de clase, el tipo de dato lo define el compilador, le da un tipo tanto a las caracteristicas
+            // del objeto como a la variable donde se inicializa el objeto
+
+            WriteLine(MiVariable.Nombre + " " + MiVariable.edad);
+
+            var miOtraVariable = new { Nombre = "Ana", edad = 25 };
+
+            MiVariable = miOtraVariable; // Puede asignarse una clase a la otra ya que son las mismas en cuanto a la cantidad de parametros y tipo de datos,
+            // si existieran mas parametros o los tipos de datos sean diferentes pasarian a ser clases distintas.
+
+            // CLASES ANÓNIMAS REQUISITOS
+
+            // - Solo pueden contener campos públicos
+            // - Todos los campos deben estar iniciados
+            // - Los campos no pueden ser static
+            // - No se pueden definir métodos
         }
 
-        static void RealizarTarea()
+        static void realizarTarea()
         {
             Punto origen = new Punto();
 
